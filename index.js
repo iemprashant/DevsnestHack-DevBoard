64c26625d333e99a5c72c476ab9de433d47a1b4f
iemprashant <prashantgupta9876@gmail.com>
2021-07-04 19:52:02 +0530

    ui updated

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const canv = document.getElementById('canvas')
 const toolbox = document.getElementById('toolbox')
 const canvascontext = canv.getContext('2d')
 const defaultcolour = document.getElementById('boardcolor').value
-var undo_arr = []
-var undo_arr_index
+var undoArray = []
+var undoArrayIndex
 var full_screen = false
 var loc = { x: 0, y: 0 }
 var controlPoint = { x: 0, y: 0 }
 var strok = false
+
 
 function init() {
     setCanvasSize()
@@ -15,12 +16,13 @@ function init() {
     canvasEventSetup()
     canv.style.cursor = 'url("./icons/pencursor.png"), auto'
     toolbox.style.cursor = 'pointer'
+    buttonStateCheck()
 }
 
 function setCanvasSize() {
     var originalsize = canvascontext.getImageData(0, 0, canv.width, canv.height)
-    canv.width = window.innerWidth - 100
-    canv.height = window.innerHeight - 100
+    canv.width = window.innerWidth - 20
+    canv.height = window.innerHeight - 90
     canvascontext.putImageData(originalsize, 0, 0, 0, 0, canv.width, canv.height)
 }
 
@@ -29,50 +31,87 @@ function setBoardColour(setcolour) {
     canvascontext.fillStyle = setcolour
     canvascontext.fillRect(0, 0, canv.width, canv.height)
 }
+
 // Board clourpicker Btn function
 function boardcolourpicker(target) {
     setBoardColour(target.value)
 }
 
-function canvasEventSetup() {
-    canv.addEventListener('mousedown', start_draw)
-    canv.addEventListener('mouseup', stop_draw)
-    canv.addEventListener('mouseup', auxillary_stop_draw)
-    canv.addEventListener('mouseout', stop_draw)
-    canv.addEventListener('pointerdown', start_draw)
-    canv.addEventListener('pointermove', draw)
-    canv.addEventListener('pointerup', stop_draw)
-    canv.addEventListener('pointerup', auxillary_stop_draw)
-    canv.addEventListener('pointerout', stop_draw)
-}
-
-// async function update_page_image() {
-//     var board_image = canvascontext.getImageData(0, 0, canv.width, canv.height)
-//     if (undo_arr_index < undo_arr.length) {
-//         undo_arr.splice(undo_arr_index + 1, undo_arr.length - undo_arr_index - 1)
-//     }
-//     undo_arr.push(board_image)
-//     undo_arr_index = undo_arr.length - 1 //array indexing issue
-//     button_state_checker()
-//         //console.log(undo_arr.length)//for testing
-// }
+// get cursor location on click`
 function cursorlocator(event) {
     loc.x = event.clientX - canv.offsetLeft
     loc.y = event.clientY - canv.offsetTop
-    console.log(`${loc.x}`)
 }
 
-async function stroke_properties(canvascontext_name) {
+function canvasEventSetup() {
+    canv.addEventListener('mousedown', start_draw)
+    canv.addEventListener('mouseup', stop_draw)
+    canv.addEventListener('mouseout', stop_draw)
+    canv.addEventListener('mouseup', updateActionToUndoArray)
+    canv.addEventListener('pointerdown', start_draw)
+    canv.addEventListener('pointermove', draw)
+    canv.addEventListener('pointerup', stop_draw)
+    canv.addEventListener('pointerup', updateActionToUndoArray)
+    canv.addEventListener('pointerout', stop_draw)
+}
+
+// working of Undo
+function buttonStateCheck() {
+    if (undoArrayIndex == 0) {
+        document.getElementById('undobtn').disabled = true
+    }
+    if (undoArrayIndex > 0) {
+        document.getElementById('undobtn').disabled = false
+    }
+    // if (undoArrayIndex == undo_arr.length - 1) {
+    //     document.getElementById('action9').disabled = true
+    // }
+    // if (undoArrayIndex < undo_arr.length - 1) {
+    //     document.getElementById('action9').disabled = false
+    // }
+    //number of undo's that can be done
+    if (undoArray.length > 3) {
+        undoArray.splice(0, undoArray.length - 3)
+    }
+}
+
+function updateActionToUndoArray() {
+    var currentState = canvascontext.getImageData(0, 0, canv.width, canv.height)
+    if (undoArrayIndex < undoArray.length) {
+        undoArray.splice(undoArrayIndex + 1, undoArray.length - undoArrayIndex - 1)
+    }
+    undoArray.push(currentState)
+    undoArrayIndex = undoArray.length - 1
+}
+
+function undo() {
+    if (undoArrayIndex > 0) {
+        undoArrayIndex -= 1
+        canvascontext.putImageData(undoArray[undoArrayIndex], 0, 0)
+    }
+    buttonStateCheck()
+}
+
+function redo() {
+    if (undoArrayIndex < undoArray.length) {
+        undoArrayIndex++
+        canvascontext.putImageData(undoArray[undoArrayIndex], 0, 0)
+    }
+    buttonStateCheck()
+}
+
+// load stroke details
+function loadStrokedetails(canvascontext_name) {
     canvascontext_name.lineCap = 'round'
     canvascontext_name.lineWidth = document.getElementById('strokewidth').value
     canvascontext_name.strokeStyle = document.getElementById('strokecolor').value
     canvascontext_name.lineJoin = 'round'
 }
-
+// drawing function
 function start_draw(event) {
     event.preventDefault()
     cursorlocator(event)
-    stroke_properties(canvascontext)
+    loadStrokedetails(canvascontext)
     strok = true
 }
 
@@ -97,10 +136,10 @@ function draw(event) {
     canvascontext.closePath()
 }
 
-function clear_page(cntx_name) {
-    cntx_name.clearRect(0, 0, canv.width, canv.height)
-    cntx_name.fillStyle = document.getElementById('boardcolor').value
-    cntx_name.fillRect(0, 0, canv.width, canv.height)
+// clear btn function
+function clearPage() {
+    canvascontext.clearRect(0, 0, canv.width, canv.height)
+    canvascontext.fillStyle = document.getElementById('boardcolor').value
+    canvascontext.fillRect(0, 0, canv.width, canv.height)
 }
-
 document.onload = init()
